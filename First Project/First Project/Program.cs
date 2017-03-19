8f105d9a1bd1391c67c89177aca4033d35a0bfae
girl-physicist <girl-physicist@mail.ru>
2017-03-20 00:19:05 +0300

    commit 2

--- First Project/First Project/Program.cs
+++ First Project/First Project/Program.cs
@@ -62,122 +62,160 @@ namespace First_Project
             //Console.WriteLine((s + v).Replace("dear", "World"));
             //Console.ReadLine();
             #endregion
-
-
             #region Conditional constructions
-            //IF - ELSE
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("IF-ELSE");
-            Console.ForegroundColor = ConsoleColor.Green;
-            if (DateTime.Now.Hour >= 6 && DateTime.Now.Hour < 12)
-            {
-                Console.WriteLine("Good MORNING");
-            }
-            else if (DateTime.Now.Hour >= 12 && DateTime.Now.Hour < 18)
-            {
-                Console.WriteLine("Good AFTERNOON");
-            }
-            else if (DateTime.Now.Hour >= 18 && DateTime.Now.Hour < 20)
-            {
-                Console.WriteLine("Good EVENING");
-            }
-            else if (DateTime.Now.Hour >= 20 && DateTime.Now.Hour < 6)
-            {
-                Console.WriteLine("Good NIGHT");
-            }
-            Console.ReadLine();
+            ////IF - ELSE
+            //Console.ForegroundColor = ConsoleColor.White;
+            //Console.WriteLine("IF-ELSE");
+            //Console.ForegroundColor = ConsoleColor.Green;
+            //if (DateTime.Now.Hour >= 6 && DateTime.Now.Hour < 12)
+            //{
+            //    Console.WriteLine("Good MORNING");
+            //}
+            //else if (DateTime.Now.Hour >= 12 && DateTime.Now.Hour < 18)
+            //{
+            //    Console.WriteLine("Good AFTERNOON");
+            //}
+            //else if (DateTime.Now.Hour >= 18 && DateTime.Now.Hour < 20)
+            //{
+            //    Console.WriteLine("Good EVENING");
+            //}
+            //else if (DateTime.Now.Hour >= 20 && DateTime.Now.Hour < 6)
+            //{
+            //    Console.WriteLine("Good NIGHT");
+            //}
+            //Console.ReadLine();
 
-            //Тернарный оператор
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("Тернарный оператор / дата");
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            var result = DateTime.Now.Hour >=18 ? "Time after 18" : "Time to 18";
-            Console.WriteLine(result);
-            Console.ReadLine();
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("Тернарный оператор / модуль числа");
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            int result2;
-            for (int i = 5; i >= -5; i--)
-            {
-                result2 = i >= 0 ? i : -i;
-                Console.Write("{0}\t", result2);
-            }
-            Console.ForegroundColor = ConsoleColor.White;
-      
-            Console.ReadLine();
-            Console.WriteLine("Тернарный оператор / четные числа");
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            for (int j=0; j<=10; j++)
-            {
-                if  (j % 2 == 0 ? true : false)                             
-            Console.Write("{0}\t", j);
-            }
-            Console.ReadLine();
+            ////Тернарный оператор
+            //Console.ForegroundColor = ConsoleColor.White;
+            //Console.WriteLine("Тернарный оператор / дата");
+            //Console.ForegroundColor = ConsoleColor.Yellow;
+            //var result = DateTime.Now.Hour >= 18 ? "Time after 18" : "Time to 18";
+            //Console.WriteLine(result);
+            //Console.ReadLine();
+            //Console.ForegroundColor = ConsoleColor.White;
+            //Console.WriteLine("Тернарный оператор / модуль числа");
+            //Console.ForegroundColor = ConsoleColor.Yellow;
+            //int result2;
+            //for (int i = 5; i >= -5; i--)
+            //{
+            //    result2 = i >= 0 ? i : -i;
+            //    Console.Write("{0}\t", result2);
+            //}
+            //Console.ForegroundColor = ConsoleColor.White;
+
+            //Console.ReadLine();
+            //Console.WriteLine("Тернарный оператор / четные числа");
+            //Console.ForegroundColor = ConsoleColor.Yellow;
+            //for (int j = 0; j <= 10; j++)
+            //{
+            //    if (j % 2 == 0 ? true : false)
+            //        Console.Write("{0}\t", j);
+            //}
+            //Console.ReadLine();
 
 
 
-            // Switch 
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("Switch-название дня");
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine("Ввеите номер дня");
-            int day =Convert.ToInt32(Console.ReadLine());
-            switch (day)
-            {
-                case 1:
-                    Console.WriteLine("Пн");
-                    break;
-                case 2:
-                    Console.WriteLine("Вт");
-                    break;
-                case 3:
-                    Console.WriteLine("Ср");
-                    break;
-                case 4:
-                    Console.WriteLine("Чт");
-                    break;
-                case 5:
-                    Console.WriteLine("Пт");
-                    break;
-                case 6:
-                    Console.WriteLine("Сб");
-                    break;
-                case 7:
-                    Console.WriteLine("Вс");
-                    break;
-                default:
-                    Console.WriteLine("Error");
-                    break;
-                        }
-           
-            
-            //Switch - выходной
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("Switch-выходной");
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine("Ввеите номер дня");
-            int day2 = Convert.ToInt32(Console.ReadLine());
-            switch (day2)
-            {
-                case 1:                   
-                case 2:            
-                case 3:                    
-                case 4:                   
-                case 5:
-                    Console.WriteLine("Рабочий");
-                    break;
-                case 6:                  
-                case 7:
-                    Console.WriteLine("Выходной");
-                    break;
-                default:
-                    Console.WriteLine("Error");
-                    break;
-            }
-            Console.ReadLine();
+            //// Switch 
+            //Console.ForegroundColor = ConsoleColor.White;
+            //Console.WriteLine("Switch-название дня");
+            //Console.ForegroundColor = ConsoleColor.Cyan;
+            //Console.WriteLine("Ввеите номер дня");
+            //int day = Convert.ToInt32(Console.ReadLine());
+            //switch (day)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Пн");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Вт");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Ср");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("Чт");
+            //        break;
+            //    case 5:
+            //        Console.WriteLine("Пт");
+            //        break;
+            //    case 6:
+            //        Console.WriteLine("Сб");
+            //        break;
+            //    case 7:
+            //        Console.WriteLine("Вс");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Error");
+            //        break;
+            //}
 
-            // 
+
+            ////Switch - выходной
+            //Console.ForegroundColor = ConsoleColor.White;
+            //Console.WriteLine("Switch-выходной");
+            //Console.ForegroundColor = ConsoleColor.Cyan;
+            //Console.WriteLine("Ввеите номер дня");
+            //int day2 = Convert.ToInt32(Console.ReadLine());
+            //switch (day2)
+            //{
+            //    case 1:
+            //    case 2:
+            //    case 3:
+            //    case 4:
+            //    case 5:
+            //        Console.WriteLine("Рабочий");
+            //        break;
+            //    case 6:
+            //    case 7:
+            //        Console.WriteLine("Выходной");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Error");
+            //        break;
+            //}
+            //Console.ReadLine();
+
+            //// Обработчик исключений
+            //Console.ForegroundColor = ConsoleColor.White;
+            //Console.WriteLine("Обработчик исключений");
+            //Console.ForegroundColor = ConsoleColor.Cyan;
+            //Console.WriteLine("Enter number");
+
+            //try
+            //{
+            //    int num = Convert.ToInt32(Console.ReadLine());
+            //    Console.WriteLine("Your number " + num);
+            //}
+            //catch (FormatException) // тип исключения (несоответствие форматов)
+            //{
+            //    Console.WriteLine("Format Exception");
+            //}
+
+
+            //Console.ReadKey();
+            #endregion
+            #region Циклы
+            ////For
+            //Console.ForegroundColor = ConsoleColor.White;
+            //Console.WriteLine("FOR");
+            //Console.ForegroundColor = ConsoleColor.Green;
+            //for (int i = 1; i < 10; i++)
+            //{ Console.Write(i +"\t");
+
+            //}
+            // Console.ReadKey();
+
+            ////While
+            //Console.ForegroundColor = ConsoleColor.White;
+            //Console.WriteLine("While");
+            //Console.ForegroundColor = ConsoleColor.Cyan;
+            //int j=0;
+            //while (j <= 5) 
+            //{
+            //    Console.Write(j + "\t");
+            //    j++;
+            //}
+            //Console.ReadKey();
             #endregion
         }
     }
