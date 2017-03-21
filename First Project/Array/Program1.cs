319c31283d0b1c59b2cb630dcd87d90c3cd6a4a5
girl-physicist <girl-physicist@mail.ru>
2017-03-21 13:47:07 +0300

    Array - Commit 2

--- First Project/Array/Program1.cs
+++ First Project/Array/Program1.cs
@@ -9,12 +9,12 @@ namespace Array
         {
             //Дан одномерный массив размером 10 и два числа P и Q (P<Q). Определить, сколько элементов массива лежит между P и Q.
 
-            2:int[] arr=new int[10];//Как это сделать меткой?
+            int[] arr = new int[10];
             Random rand = new Random();
-          // Array filling
+            // Array filling
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("Choose how to fill the array.");
-            Console.Write("\n"+"Enter the value from the keyboard. Enter ");
+            Console.Write("\n" + "Enter the value from the keyboard. Enter ");
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("1");
             Console.ForegroundColor = ConsoleColor.White;
@@ -41,44 +41,78 @@ namespace Array
                 }
             }
             // Input P and Q
-           Console.WriteLine();
-      1: Console.WriteLine("Enter P and Q (P < Q)");
-          int P = int.Parse(Console.ReadLine());//выбрасывает??????????
-          int  Q = int.Parse(Console.ReadLine());
-            if (P < Q)
-                Console.WriteLine("Your number P=" + P + " Your number Q= " + Q);
-            else
+            //    Console.WriteLine();
+            //Console.WriteLine("Enter P and Q (P < Q)");
+            //     string inputP = Console.ReadLine();
+            //     int P;
+            //     Int32.TryParse(inputP, out P);
+            //     string inputQ = Console.ReadLine();
+            //     int Q;
+            //     Int32.TryParse(inputQ, out Q);
+            //     if (P < Q)
+            //         Console.WriteLine("Your number P=" + P + " Your number Q= " + Q);
+            //     else
+            //     {
+            //         Console.WriteLine("You entered P>=Q. Please, Enter P and Q (P<Q) ");
+
+            //     }
+
+            var P = 0;
+            var Q = 0;
+            while (P >= Q)
             {
-                Console.WriteLine("You entered P>=Q. Please, Enter P and Q (P<Q) ");
-                goto 1;
+                Console.WriteLine("Enter P and Q (P < Q)");
+                Console.WriteLine("P:");
+                if (int.TryParse(Console.ReadLine(), out P))
+                {
+                    Console.WriteLine("Q:");
+                    if (int.TryParse(Console.ReadLine(), out Q))
+                    {
+                        if (P >= Q)
+                        {
+                            Console.WriteLine("P should be less than Q");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Q must be number");
+                        P = 0;
+                    }
+                }
+                else
+                    Console.WriteLine("P must be number");
             }
-           
 
 
-            // The number of elements between P and Q
-           int a = 0;
-           for (int j = 0; j < arr.Length; j++)
-                if (P < arr[j] && Q > arr[j])
-                    a += 1;
-            Console.WriteLine("Между элементами лежит " + a + " элеметнов");
-            Console.WriteLine(" ");
 
-            Console.WriteLine("Для закрытия консоли нажмите 'Y', Если вы хотите перезапустить скрипт, нажмите 'N'");
-            if (Console.ReadKey().Key == ConsoleKey.N)
-            {
-                Console.WriteLine();
-                
+
+
+
+                // The number of elements between P and Q
+                int a = 0;
+                for (int j = 0; j < arr.Length; j++)
+                    if (P < arr[j] && Q > arr[j])
+                        a += 1;
+                Console.WriteLine("Между элементами лежит " + a + " элеметнов");
+                Console.WriteLine(" ");
+
+                Console.WriteLine("Для закрытия консоли нажмите 'Y', Если вы хотите перезапустить скрипт, нажмите 'N'");
+                if (Console.ReadKey().Key == ConsoleKey.N)
+                {
+                    Console.WriteLine();
+
+                }
+                else
+                { }
+
+
+
+
+
+
+
+
             }
-           else
-                goto 2;
-
-
-
-
-
-
-
-
         }
     }
-}
+
